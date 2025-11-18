21b7874eb8e690ad1c03cd7b30c817c7ee6c0d09
Aman <amankumarpandey1712004@gmail.com>
2025-11-18 23:44:42 +0530

    signup and signin feature added

--- oops_proj.py
+++ oops_proj.py
@@ -13,9 +13,9 @@ class chatbook:
                            4. Press 4 to message a friend
                            5. Press any other key to exit""")
         if user_input == "1":
-            pass
+            self.signup()
         elif user_input == "2":
-            pass
+            self.signin()
         elif user_input == "3":
             pass
         elif user_input == "4":
@@ -23,6 +23,27 @@ class chatbook:
         else:
             exit()   
 
+    def signup(self):
+        email = input("Enter your email: ")
+        pwd = input("Enter your password: ")
+        self.username = email
+        self.password = pwd
+        print("your have signup successfully !!")
+        print("\n")
+        self.menu()
+    def signin(self):
+        if self.username=='' and self.password=='':
+            print("No user found, please signup first")
+        else:
+            uname = input("enter your email/username here-> ")
+            pwd = input("Enter your password here -> ")
+            if self.username==uname and self.password==pwd:
+                print("you have signed in successfully !!")
+                self.loggedin =True
+            else:
+                print("Please input correct credentials...")
 
+        print("\n")
+        self.menu()
 
 obj = chatbook()
